1c4b531921421a28bee3fad4098778bdf688482d
Pulin Agrawal <pulinagrawal@gmail.com>
2024-03-29 11:25:08 -0400

    current progress

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from utils import prepare_data
 from sklearn.metrics import accuracy_score
 
 
-def goodness_score(pos_acts, neg_acts, threshold=4):
+def goodness_score(pos_acts, neg_acts, threshold=1):
     """
     Compute the goodness score for a given set of positive and negative activations.
 
@@ -26,9 +26,10 @@ def goodness_score(pos_acts, neg_acts, threshold=4):
     quantity but without the threshold subtraction
     """
 
-    pos_goodness = -torch.sum(torch.pow(pos_acts, 2)) + threshold
-    neg_goodness = torch.sum(torch.pow(neg_acts, 2)) - threshold
-    return torch.add(pos_goodness, neg_goodness)
+    pos_goodness = -torch.sigmoid(torch.sum(torch.pow(pos_acts, 2)) + threshold)
+    neg_goodness = torch.sigmoid(torch.sum(torch.pow(neg_acts, 2)) - threshold)
+    norm_ratio = torch.norm(pos_acts, dim=0, p=1)/torch.norm(pos_acts, dim=0, p=2)
+    return torch.add(pos_goodness, neg_goodness)#+torch.std(pos_acts)+torch.std(neg_acts)
 
 
 def get_metrics(preds, labels):
@@ -101,6 +102,8 @@ class Unsupervised_FF(nn.Module):
                     pos_acts = layer(pos_acts)
                     neg_acts = layer(neg_acts)
                     layer.ff_train(pos_acts, neg_acts)
+                    # pos_acts = layer.ln_layer(pos_acts.detach())
+                    # neg_acts = layer.ln_layer(neg_acts.detach())
 
     def train_last_layer(self, dataloader: DataLoader):
         num_examples = len(dataloader)
